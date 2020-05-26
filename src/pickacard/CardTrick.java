d82e0bce1e5f4d0b4514e1bde739a38d34829802
asun975 <sunas@sheridancollege.ca>
2020-05-26 18:28:03 -0400

    commit 2 : setValue to random number

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,8 +1,9 @@
 package pickacard;
 
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
- * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
  *
  * @author dancye
  * @author Paul Bonenfant
@@ -11,12 +12,14 @@ package pickacard;
 public class CardTrick {
 
     public static void main(String[] args) {
-        
+
         Card[] magicHand = new Card[7];
 
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            double random = 1 + Math.random() * 13;
+            c.setValue((int)(random));
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
 
