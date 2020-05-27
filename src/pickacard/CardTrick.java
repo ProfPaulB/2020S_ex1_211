206bd2b858f1a62446315c82907726f9ac9316d3
Varun <Varun@Varun>
2020-05-27 16:01:05 -0400

    creating scanner and checking

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -7,21 +7,52 @@ package pickacard;
  * @author dancye
  * @author Paul Bonenfant
  */
+import java.util.Scanner;
+
 public class CardTrick {
 
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+        
         Card[] magicHand = new Card[7];
+        
+        boolean win = false;
 
         for (int i = 0; i < magicHand.length; i++) {
+        
             Card c = new Card();
+            
             //c.setValue(insert call to random number generator here)
+            c.setValue((int) (Math.random() * 13 + 1));
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[(int) Math.random() * 4 ]);
+            magicHand[i] = c;
         }
 
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("Enter Card Number: ");
+        
+        int crdNo = sc.nextInt();
+        
+        System.out.println("Enter Suit: ");
+        
+        String suit = sc.nextLine();
         // and search magicHand here
+        for (int i = 0; i < magicHand.length; i++) {
+            
+            if ((magicHand[i].getValue() == crdNo) &&
+                (magicHand[i].getSuit() == suit)) {
+            
+                win = true;
+        }
+        }
+        
         //Then report the result here
+        if (win == true){
+            System.out.println("You Win!");
+        }else
+            System.out.println("You Lose!");
     }
 
 }
