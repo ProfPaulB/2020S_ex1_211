e23ecd7672509b1c95f42abdde4a93721a486e20
merenarslan <arslamel@sheridancollege.ca>
2020-05-27 10:40:44 -0400

    Added comments

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -15,39 +15,39 @@ public class CardTrick {
 
     public static void main(String[] args) {
         
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); // scanner object
         Card[] magicHand = new Card[7];
         
     
-        System.out.println("Please enter a value: ");
-        int crdValue = Integer.valueOf(input.nextLine());
+        System.out.println("Please enter a value");
+        int crdValue = Integer.valueOf(input.nextLine()); //input for card value
         System.out.println("Please choose a suit (exp. 1) :");
         System.out.println("1.Hearts\n"
                 + "2.Diamonds\n"
                 + "3.Spades\n"
                 + "4.Clubs");
-        int suitValue = Integer.valueOf(input.nextLine());      
+        int suitValue = Integer.valueOf(input.nextLine()); // input for card suit      
         
                 
     
         
-        int matching = 0;
+        int matching = 0; // int if person card matheches with random numbers 
         for (int i = 0; i < magicHand.length; i++) {            
             Card c = new Card();
-            int rnd = (int)(Math.random()*((13+1)-1))+1;
+            int rnd = (int)(Math.random()*((13+1)-1))+1; // getting random numbers between 0-13 for the card number
             c.setValue(rnd); //(insert call to random number generator here)
-            int rndsuit = (int)(Math.random()*((3-0)+1));
-            //c.setSuit(); //[insert call to random number between 0-3 here]
-            String suits = c.SUITS[rndsuit];
-            c.setSuit(suits);
-            System.out.println(c.getValue());
-            System.out.println(c.getSuit());
-            if(rnd == crdValue && suitValue == rndsuit){
-                matching =+1;                
+            int rndsuit = (int)(Math.random()*((3-0)+1)); // getting random numbers between 0-3 for suit array.           
+            String suits = c.SUITS[rndsuit]; //calling suit array
+            c.setSuit(suits); //[insert call to random number between 0-3 here]
+            System.out.println(c.getValue()); // print out random card number
+            System.out.println(c.getSuit());  // print out random suit 
+            if(rnd == crdValue && suitValue == rndsuit){ // 
+                matching =+1; // adding one if person's choice matches with random numbers            
                 
             }
             
         }
+        //printing out the final report
         if(matching > 0){
         System.out.println("Your card is in the magic hand...");
         }else{
@@ -55,11 +55,7 @@ public class CardTrick {
         }
         
 
-        //insert code to ask the user for Card value and suit, create their card
-        
-        
-        // and search magicHand here
-        //Then report the result here
+       
     }
 
 }
