6a889ae937ce70da807696985d65aca8010d9c2a
HaominChen <chehaomi@sheridancollege.ca>
2020-05-27 16:21:30 -0400

    Completed Ex1 Task 7. Merged the code and replaced the user code with luckyCard object and tested it. And comment out the testing code.

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,6 +1,5 @@
 package pickacard;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -28,52 +27,29 @@ public class CardTrick {
             magicHand[i] = c;
         }
 
-        //Only for testing, show all the magicHand array result 
-        for (Card c : magicHand) {
-            System.out.printf("Card value/suit: %d/%s%n", c.getValue(), c.getSuit());
-        }
-        
+//        //Only for testing, show all the magicHand array result 
+//        for (Card c : magicHand) {
+//            System.out.printf("Card value/suit: %d/%s%n", c.getValue(), c.getSuit());
+//        }
+
         //Complete Task 6: add a luckyCard object and assign the value to 5 and "Hearts"
         Card luckyCard = new Card();
         luckyCard.setValue(5);
         luckyCard.setSuit("Hearts");
-        
-        /**
-         * Complete task 4 part1: ask the user for the card value and suit and
-         * check data validation
-         */
-        //insert code to ask the user for Card value and suit, create their card    
-        // and search magicHand here
-        try {
-            System.out.println("Pick one card nunmber from 1 to 13, Ace = 1,"
-                    + "Jack = 11, Queen = 12, King = 13:");
-            int userNum = in.nextInt();
-            if (userNum > 13 || userNum < 1) {
-                throw new Exception("Error: one card number should be from 1 to 13.");
-            }
-            System.out.println("Pick one card suit from 0 to 3, 0= Hearts,1= "
-                    + "Diamonds, 2=Spades, 3=Clubs:");
-            int suitNum = in.nextInt();
-            if (suitNum > 3 || suitNum < 0) {
-                throw new Exception("Error: one card suit number should be from 0 to 3.");
-            }
-            String userSuit = Card.SUITS[suitNum];
 
-            //Complete task 4 part2: report to the user about the result
-            //Then report the result here
-            boolean isMatched = true;
-            for (Card c : magicHand) {
-                if (userNum == c.getValue() && userSuit.equals(c.getSuit())) {
-                    isMatched = true;
-                    break;
-                } else {
-                    isMatched = false;
-                }
+        //Complete task 7: report the luckyCard result
+        //Then report the result here
+        boolean isMatched = true;
+        for (Card c : magicHand) {
+            if (luckyCard.getValue() == c.getValue()
+                    && luckyCard.getSuit().equals(c.getSuit())) {
+                isMatched = true;
+                break;
+            } else {
+                isMatched = false;
             }
-            String message = (isMatched) ? "Congratulations! You win!" : "Sorry! You lose!";
-            System.out.println(message);
-        } catch (InputMismatchException ex) {
-            System.out.println("Error: the input should be a number");
         }
+        String message = (isMatched) ? "Congratulations! You win!" : "Sorry! You lose!";
+        System.out.println(message);
     }
 }
