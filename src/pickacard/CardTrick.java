490f5399759046dd2042ae5f0aee821b3c80085b
ceccoj <65797404+ceccoj@users.noreply.github.com>
2020-05-27 10:20:15 -0400

    Changed to a set lucky card

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,7 +1,5 @@
 package pickacard;
 
-import java.util.Scanner;
-
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then
  * asks the user to pick a card and searches the array of cards for the match to
@@ -16,40 +14,33 @@ public class CardTrick {
     public static void main(String[] args) {
 
         Card[] magicHand = new Card[7];
-        Scanner input = new Scanner(System.in);
         boolean isWinner = false;
+        Card luckyCard = new Card();
 
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
             c.setValue((int) (Math.random() * 13 + 1));
             c.setSuit(Card.SUITS[(int) (Math.random() * 4)]);
             magicHand[i] = c;
-            // Printing to test code to ensure random function working as needed
+            /* Printing to test code to ensure random function working as needed
             System.out.println(c.getValue());
             System.out.println(c.getSuit());
-             
+            */
         }
 
-        //insert code to ask the user for Card value and suit, create their card
-        Card playerCard = new Card();
-        System.out.println("Enter a card value between 1 and 13. (Ace = 1, "
-                + "Jack = 11, Queen = 12, King = 13): ");
-        playerCard.setValue(input.nextInt());
-        System.out.println("Enter a card suit (Hearts, Diamonds, Clubs, Spades): ");
-        playerCard.setSuit(input.next());
+        //setting values for luckyCard
+        luckyCard.setValue(10);
+        luckyCard.setSuit("Spades");
 
         // and search magicHand here
         for (int i = 0; i < magicHand.length; i++) {
-            if (playerCard.getValue() == magicHand[i].getValue()) {
+            if (luckyCard.getValue() == magicHand[i].getValue()) {
                 //searching the second value only if there is a match in the first
-                if (playerCard.getSuit().equalsIgnoreCase(magicHand[i].getSuit())) {
+                if (luckyCard.getSuit().equalsIgnoreCase(magicHand[i].getSuit())) {
                     isWinner = true;
                 }
             }
         }
-        /* testing magicHand array
-        System.out.println(magicHand[0].getValue() + " " + magicHand[0].getSuit());
-        */
         
         //Then report the result here
         String format = (isWinner) ? "You win!" : "Sorry, better luck next time";
