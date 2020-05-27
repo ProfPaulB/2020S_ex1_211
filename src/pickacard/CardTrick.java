b1c23a312afce21801e8e8932a39d217f2b3f68b
andrew-ng-248 <ngyi@sheridancollege.ca>
2020-05-27 01:32:19 -0400

    First complete draft

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -3,7 +3,7 @@ package pickacard;
 import java.util.Scanner;
 
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
+ * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and 
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
  * @author dancye
@@ -14,7 +14,7 @@ public class CardTrick {
 
     public static void main(String[] args) {
 
-        // number of cards as a variable makes the code flexible
+        // using a variable for # of cards makes the code flexible
         int numOfCards = 7;
         Card[] magicHand = new Card[numOfCards];
 
@@ -33,16 +33,46 @@ public class CardTrick {
         }
 
         // For Testing: printing out magicHand array
-        System.out.println("Hand of cards is assigned");
+        System.out.println("The hand of cards is:");
         for (Card card : magicHand) {
             System.out.println(card.getValue() + " of " + card.getSuit());
         }
-        
-        
 
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        // ask the user for Card value and suit, and store as a Card object
+        Scanner input = new Scanner(System.in);
+        Card userCard = new Card();
+        System.out.println("***Pick a card by its value and suit.\n");
+        
+        // prompt user for card value, and set the value attribute
+        System.out.println("Value of your card:");
+        System.out.println("Enter the actual number for values 2 to 10."); 
+        System.out.println("Enter 1 for Ace, 11 for Jack, 12 for Queen, and 12 for King."); 
+        System.out.print("Enter a number for the value of your card:");
+        userCard.setValue(input.nextInt());
+        
+        // prompt user for card suit, and set the suit attribute
+        System.out.println("***Suit of your card:");
+        System.out.println("Enter 0 for Hearts, 1 for Diamonds, 2 for Spades, and 3 for Clubs.");
+        System.out.print("Enter a number for the suit of your card:");
+        userCard.setSuit(Card.SUITS[input.nextInt()]);
+        
+        // For Testing: printing out magicHand array
+        System.out.println("Your card is " + userCard.getValue() + " of " + userCard.getSuit());
+        
+        // search through magicHand Card array for matches to user's Card
+        boolean match = false;
+        
+        for (Card card: magicHand) {
+            if (card.getValue() == userCard.getValue() && card.getSuit().equals(userCard.getSuit()) )
+                match = true;
+        }
+        
+        // report the result of the match search to user
+        if (match)
+            System.out.println("You win! Your chosen card is in the magic hand of random cards.");
+        else
+            System.out.println("You lose. Your chosen card is not in the magic hand of random cards.");
+
     }
 
 }
