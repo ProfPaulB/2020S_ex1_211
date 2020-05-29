bdae50e6755aaaaa9b22f50ed64b0f547e38307b
alsdn9342 <parkminw@sheridancollege.ca>
2020-05-28 23:24:26 -0400

    test

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
  */
 public class CardTrick {
 
-    /**xxxxxx
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
