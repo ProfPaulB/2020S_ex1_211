af073b5940955a7d2dd2f17a3dfac204df364b98
RicardoGAlonso <ricardogalonso@hotmail.com>
2020-06-01 14:50:49 -0400

    Create toString for debug

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,8 +1,11 @@
 package pickacard;
 
+import java.util.Random;
+
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
- * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
  *
  * @author dancye
  * @author Paul Bonenfant
@@ -10,11 +13,21 @@ package pickacard;
 public class CardTrick {
 
     public static void main(String[] args) {
-        
-        Card[] magicHand = new Card[7];
 
-        for (int i = 0; i < magicHand.length; i++) {
+        Random rand = new Random();
+        Card[] magicHand = new Card[7];
+        String suitx;
+
+        //for (int i = 0; i < magicHand.length; i++) {
+        for (Card magicHand1 : magicHand) {
             Card c = new Card();
+            int r;
+            r = rand.nextInt(4);
+            suitx = Card.SUITS[r];
+            c.setSuit(suitx);
+            r = rand.nextInt(13) + 1;
+            c.setValue(r);
+            //System.out.println(c);
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
@@ -23,5 +36,6 @@ public class CardTrick {
         // and search magicHand here
         //Then report the result here
     }
+
 
 }
